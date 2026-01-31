871f3349af72667a10cb40b473ddc59264af068f
AxNxY <101530751+AxNxY@users.noreply.github.com>
2026-01-31 23:58:03 +0300

    Updating With Patch Requests

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -11,12 +11,15 @@
         <p><?= htmlspecialchars($note['body'])?> </p>
         <!--htmlspecialchars → XSS saldırılarını önlemek için -->
 
-        <form class="mt-6" method="POST">
-            <!-- Form POST ile gönderiliyor Action yazmadığı için: 👉 Aynı sayfaya geri gönderiliyor -->
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="id" value="<?= $note['id'] ?>">
-            <button class="text-sm text-red-500"> Delete </button>
-        </form>
+        <footer class="mt-6">
+            <a href="/note/edit?id=<?= $note['id'] ?>" class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</a>
+        </footer>
+<!--        <form class="mt-6" method="POST">-->
+<!--          Form POST ile gönderiliyor Action yazmadığı için: 👉 Aynı sayfaya geri gönderiliyor -->
+<!--            <input type="hidden" name="_method" value="DELETE">-->
+<!--            <input type="hidden" name="id" value="-<?php //= $note['id'] ?>" -->
+<!--            <button class="text-sm text-red-500"> Delete </button>-->
+<!--        </form>-->
     </div>
 </main>
 
